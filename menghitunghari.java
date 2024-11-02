14c69f58eedbde7ded5567027475c40585326b3e
petirrozora <fathir.ilham2405@gmail.com>
2024-11-03 06:44:34 +0700

    first commit

---
+++ menghitunghari.java
@@ -0,0 +1,33 @@
+
+package Praktikum_10_PBO;
+
+/**
+ *
+ * @author Crizo
+ */
+public class menghitunghari {
+     public String Hitung(String tahun, String bulan){
+        String[] bln31 = {"Januari", "Maret","Mei","Juli","Agustus","Oktober","Desember"};
+        int getTahun = Integer.valueOf(tahun);
+        String getHari = "";
+        for (String i : bln31) {
+            if(bulan == i){
+                getHari = "31";
+                break;
+            }
+        }
+        if(getHari == ""){
+            if(bulan == "Februari"){
+                if(getTahun % 4 == 0){
+                    getHari = "29";
+                }else{
+                    getHari = "28";
+                }
+            }else{
+                getHari = "30";
+            }
+        }
+            
+        return "Jumlah hari pada bulan "+bulan+" tahun "+ tahun +" adalah "+getHari;
+     }
+}
